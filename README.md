304a9924f6f5f9b891bc965e8c84d3d41ed6517a
M1Yellow <m1yellow@163.com>
2024-05-18 08:07:51 +0800

    javaagent&javessist 学习，首次提交

---
+++ README.md
@@ -0,0 +1,21 @@
+## 说明
+
+使用 `Javaagent` + `Javassist` 实现动态修改类方法。
+
+可应用在 IntelliJ IDEA 30 天试用到期后，自动去掉 Licenses（许可证）弹窗，达到【无限试用】的效果。
+
+目前仅短期测试了 `IntelliJ IDEA 2023.3.6` 版本正常使用，其他版本请自行测试。
+
+本项目**仅供技术参考学习**！有条件的**请支持正版**或申请优惠渠道！
+
+<br/>
+## 使用
+
+1. 下载部署 Oracle JDK 17 / Open JDK 17 环境
+2. 下载配置 Maven 3.6.3 （或以上版本）
+3. `git clone url` 克隆项目到本地
+4. IDEA 打开项目，Maven package 打包
+5. target 目录下的 `doidea-1.0.0-jar-with-dependencies.jar` 即为可用 jar 包，复制到一个目录，比如：`E:\DevRes\doidea`
+6. `idea64.exe.vmoptions` 添加 `-javaagent:E:\DevRes\doidea\doidea-1.0.0-jar-with-dependencies.jar` 即可生效
+
+<br/>
---
+++ src/main/java/com/doidea/core/Launcher.java
@@ -0,0 +1,115 @@
+package com.doidea.core;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.Modifier;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+public class Launcher {
+
+    private static volatile boolean loaded = false;
+
+    public static void main(String[] args) {
+        // 程序自身的日志不会在IDEA日志中打印
+        System.out.println(">>>> Launcher main >>>>");
+    }
+
+    /**
+     * 在 JVM 启动前加载
+     *
+     * @param args
+     * @param instrumentation
+     */
+    public static void premain(String args, Instrumentation instrumentation) {
+
+        if (loaded) {
+            System.err.println(">>>> multiple javaagent jar.");
+            return;
+        }
+
+        try {
+            MyClassFileTransformer transformer = new MyClassFileTransformer();
+            instrumentation.addTransformer(transformer);
+            //instrumentation.addTransformer(transformer, true);
+            loaded = true;
+        } catch (Exception e) {
+            System.err.println(">>>> instrumentation addTransformer failed.");
+            e.printStackTrace();
+        }
+    }
+
+    public static class MyClassFileTransformer implements ClassFileTransformer {
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+
+            //System.out.println(">>>> Class 已加载：" + className);
+            String modifyClassName = "com.intellij.openapi.ui.DialogWrapper";
+            String modifyClassMethod = "setTitle";
+            //String modifyClassName = "com.intellij.openapi.ui.impl.DialogWrapperPeerImpl$MyDialog";
+            //String modifyClassMethod = "getDialogWrapper"; // final class 中的方法也能改
+            String loadClassName = modifyClassName.replace(".", "/");
+            // 表示找到了这个类
+            //if (className.contains("DialogWrapperPeerImpl")) {
+            if (className.equals(loadClassName)) {
+                System.out.println(">>>> Target Class: " + "0000");
+                System.out.println(">>>> Target Class: " + className);
+                // 开始使用当前的javassist修改字节码文件
+                try {
+                    System.out.println(">>>> Target Class: " + "0001");
+                    /*
+                    TODO ClassPool.getDefault() 需要依赖 javassist jar 包，否则这个语句不执行，也不报错！！
+                    MANIFEST.MF 中指定 lib/javassist-3.30.2-GA.jar 无效
+                    使用 maven-assembly-plugin 打包
+                    或者 JVM 启动参数添加：
+                    -Xbootclasspath/a:E:\DevRes\doidea\javassist-3.30.2-GA.jar
+                     */
+                    ClassPool cp = ClassPool.getDefault();
+                    System.out.println(">>>> Target Class: " + "0001.1");
+                    CtClass cc = cp.get(modifyClassName); // xxx.xxxx.xxx xxx.xxxx.xxx$xxx 格式
+                    System.out.println(">>>> Target Class: " + "0001.2");
+                    // 判断类是否被冻结
+                    if (cc.isFrozen()) {
+                        System.out.println(">>>> Target Class isFrozen.");
+                        cc.defrost();
+                    }
+                    System.out.println(">>>> Target Class: " + "0002");
+                    //CtMethod declaredMethod = cc.getDeclaredMethod(modifyClassMethod, new CtClass[]{CtClass.charType});
+                    CtMethod declaredMethod = cc.getDeclaredMethod(modifyClassMethod);
+                    // 设置访问权限
+                    declaredMethod.setModifiers(Modifier.PUBLIC);
+                    //cc.removeMethod(declaredMethod);
+
+                    System.out.println(">>>> Target Class: " + "0003");
+                    String mBody = "{com.intellij.openapi.ui.DialogWrapper dw = this.myDialogWrapper.get();\n"
+                            + "System.out.println(\">>>> DialogWrapper title: \" + dw.getTitle());\n"
+                            + "if (dw.getTitle().equalsIgnoreCase(\"Licenses\") || dw.getTitle().equalsIgnoreCase(\"许可证\")) {return null};\n"
+                            + "return dw;\n}";
+
+                    System.out.println(">>>> Target Class mBody: " + mBody);
+                    //declaredMethod.setBody(mBody);
+                    declaredMethod.insertBefore("{String title = $1;\nSystem.out.println(title);\nif (title.equalsIgnoreCase(\"Licenses\") || title.equalsIgnoreCase(\"许可证\")) {throw new RuntimeException(\"Licenses dialog abort.\");}}");
+                    // detach 将内存中曾经被javassist加载过的CtClass类对象移除，下次调用重新走javassist加载
+                    cc.detach();
+                    System.out.println(">>>> Target Class: " + "0004");
+                    return cc.toBytecode();
+                } catch (Exception e) {
+                    System.err.println(">>>> do transform error: " + e.getMessage());
+                    e.printStackTrace();
+                } catch (Throwable e) { // 捕获 ClassPool.getDefault() 异常
+                    System.err.println(">>>> do transform error: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            //return ClassFileTransformer.super.transform(loader, className, classBeingRedefined, protectionDomain, classfileBuffer);
+            //return classfileBuffer;
+            // 返回null则字节码不会被修改
+            return null;
+        }
+    }
+}
